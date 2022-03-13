e3bdf4285484f4b86fd956aa3a12a9c99a9c8e6f
haruharu9000 <nitori4baime@gmail.com>
2022-03-13 18:43:52 +0900

    first commit

---
+++ src/components/Date.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-space direction="vertical">
+    <a-date-picker v-model:value="value1" />
+    <a-month-picker v-model:value="value2" placeholder="Select month" />
+    <a-range-picker v-model:value="value3" />
+    <a-week-picker v-model:value="value4" placeholder="Select week" />
+  </a-space>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  setup() {
+    return {
+      value1: ref(),
+      value2: ref(),
+      value3: ref([]),
+      value4: ref(),
+    };
+  },
+});
+</script>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { DatePicker } from "ant-design-vue";
+// import "ant-design-vue/lib/date-picker/style/css"; // for css
+import "ant-design-vue/dist/antd.css";
+
+const app = createApp(App);
+
+app.use(DatePicker);
+app.mount("#app");
